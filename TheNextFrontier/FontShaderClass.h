5ece6d5665f2f68573b2011888bce4a3a6815d69
Johan Karlsson <JohanX.Karlsson@Gmail.com>
2018-06-07 22:29:23 +0200

    Added FontShaderClass to handle the shader for the UI Font to be rendered on the screen

---
+++ TheNextFrontier/FontShaderClass.h
@@ -0,0 +1,54 @@
+#ifndef _FONTSHADERCLASS_H_
+
+#define _FONTSHADERCLASS_H_
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <fstream>
+
+using namespace DirectX;
+using namespace std;
+
+class FontShaderClass
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct PixelBufferType
+	{
+		XMFLOAT4 pixelColor;
+	};
+
+public:
+	FontShaderClass();
+	FontShaderClass(const FontShaderClass&);
+	~FontShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+	void RenderShaders(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader * mVertexShader;
+	ID3D11PixelShader* mPixelShader;
+	ID3D11InputLayout* mLayout;
+	ID3D11Buffer* mMatrixBuffer;
+	ID3D11SamplerState* mSampleState;
+	ID3D11Buffer* mPixelBuffer;
+};
+
+#endif

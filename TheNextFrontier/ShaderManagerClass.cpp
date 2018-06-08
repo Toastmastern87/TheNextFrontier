5377a4951a8b4a4f4c842c0194638cce4bb052da
Johan Karlsson <JohanX.Karlsson@Gmail.com>
2018-06-08 12:48:45 +0200

    Added ShaderManagerClass to handle all shaders in the future

---
+++ TheNextFrontier/ShaderManagerClass.cpp
@@ -0,0 +1,75 @@
+#include "ShaderManagerClass.h"
+
+ShaderManagerClass::ShaderManagerClass() 
+{
+	mColorShader = 0;
+	mFontShader = 0;
+}
+
+ShaderManagerClass::ShaderManagerClass(const ShaderManagerClass& other)
+{
+}
+
+ShaderManagerClass::~ShaderManagerClass() 
+{
+}
+
+bool ShaderManagerClass::Initialize(ID3D11Device* device, HWND hwnd)
+{
+	bool result;
+
+	mColorShader = new ColorShaderClass;
+	if (!mColorShader)
+	{
+		return false;
+	}
+
+	result = mColorShader->Initialize(device, hwnd);
+	if (!result)
+	{
+		return false;
+	}
+
+	mFontShader = new FontShaderClass;
+	if (!mFontShader)
+	{
+		return false;
+	}
+
+	result = mFontShader->Initialize(device, hwnd);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ShaderManagerClass::Shutdown() 
+{
+	if (mFontShader) 
+	{
+		mFontShader->Shutdown();
+		delete mFontShader;
+		mFontShader = 0;
+	}
+
+	if (mColorShader)
+	{
+		mColorShader->Shutdown();
+		delete mColorShader;
+		mColorShader = 0;
+	}
+
+	return;
+}
+
+bool ShaderManagerClass::RenderColorShader(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix)
+{
+	return mColorShader->Render(deviceContext, indexCount, worldMatrix, viewMatrix, projectionMatrix);
+}
+
+bool ShaderManagerClass::RenderFontShader(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, XMFLOAT4 color)
+{
+	return mFontShader->Render(deviceContext, indexCount, worldMatrix, viewMatrix, projectionMatrix, texture, color);
+}
---
+++ TheNextFrontier/ShaderManagerClass.h
@@ -0,0 +1,26 @@
+#ifndef _SHADERMANAGERCLASS_H_
+#define _SHADERMANAGERCLASS_H_
+
+#include "D3DClass.h"
+#include "ColorShaderClass.h"
+#include "FontShaderClass.h"
+
+class ShaderManagerClass 
+{
+public:
+	ShaderManagerClass();
+	ShaderManagerClass(const ShaderManagerClass&);
+	~ShaderManagerClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+
+	bool RenderColorShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX);
+	bool RenderFontShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+
+private:
+	ColorShaderClass* mColorShader;
+	FontShaderClass* mFontShader;
+};
+
+#endif

6859024f53240612bab225ee6e3788b51b934b9b
Johan Karlsson <JohanX.Karlsson@Gmail.com>
2018-06-25 12:36:02 +0200

    Added a class to handle the game time, not implemented in the rest of the game tho

---
+++ TheNextFrontier/GameTimeClass.cpp
@@ -0,0 +1,120 @@
+#include "GameTimeClass.h"
+
+GameTimeClass::GameTimeClass()
+{
+}
+
+GameTimeClass::GameTimeClass(const GameTimeClass& other)
+{
+}
+
+GameTimeClass::~GameTimeClass()
+{
+}
+
+void GameTimeClass::Initialize()
+{
+	mGameTimeMS = 0;
+	mGameTimeSecs = 0;
+	mGameTimeMins = 0;
+	mGameTimeHours = 0;
+	mGameTimeDays = 0;
+	mGameTimeYears = 0;
+
+	mSpeed = 1;
+
+	mOldTime = timeGetTime();
+
+	return;
+}
+
+void GameTimeClass::Frame()
+{
+	mGameTimeMS += (timeGetTime() - mOldTime) * mSpeed;
+
+	mOldTime = timeGetTime();
+
+	if (mGameTimeMS >= 1000)
+	{
+		mGameTimeMS -= 1000;
+
+		mGameTimeSecs++;
+	}
+
+	if (mGameTimeSecs >= 60)
+	{
+		mGameTimeSecs -= 60;
+
+		mGameTimeMins++;
+	}
+
+	if (mGameTimeMins >= 60)
+	{
+		mGameTimeMins -= 60;
+
+		mGameTimeHours++;
+	}
+
+	if ((mGameTimeHours >= 24) && (mGameTimeMins >= 35) && (mGameTimeSecs >= 39))
+	{
+		mGameTimeHours -= 24;
+
+		mGameTimeDays++;
+	}
+
+	if (mGameTimeDays >= 687)
+	{
+		mGameTimeDays -= 687;
+
+		mGameTimeYears++;
+	}
+
+	return;
+}
+
+int GameTimeClass::GetGameTimeMS()
+{
+	return mGameTimeMS;
+}
+
+int GameTimeClass::GetGameTimeSecs()
+{
+	return mGameTimeSecs;
+}
+
+int GameTimeClass::GetGameTimeMins()
+{
+	return mGameTimeMins;
+}
+
+int GameTimeClass::GetGameTimeHours()
+{
+	return mGameTimeHours;
+}
+
+int GameTimeClass::GetGameTimeDays()
+{
+	return mGameTimeDays;
+}
+
+int GameTimeClass::GetGameTimeMarsYears()
+{
+	return mGameTimeMarsYears;
+}
+
+void GameTimeClass::IncreaseGameSpeed()
+{
+	if (mSpeed <= 100)
+	{
+		mSpeed *= 10;
+	}
+}
+
+void GameTimeClass::DecreaseGameSpeed()
+{
+	if (mSpeed > 0)
+	{
+		mSpeed /= 10;
+	}
+
+}
---
+++ TheNextFrontier/GameTimeClass.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+#pragma comment(lib, "winmm.lib")
+
+#include <Windows.h>
+#include <mmsystem.h>
+
+class GameTimeClass
+{
+public:
+	GameTimeClass();
+	GameTimeClass(const GameTimeClass&);
+	~GameTimeClass();
+
+	void Initialize();
+	void Frame();
+	int GetGameTimeMS();
+	int GetGameTimeSecs();
+	int GetGameTimeMins();
+	int GetGameTimeHours();
+	int GetGameTimeDays();
+	int GetGameTimeMarsYears();
+	void IncreaseGameSpeed();
+	void DecreaseGameSpeed();
+
+private:
+	int mGameTimeMS, mGameTimeSecs, mGameTimeMins, mGameTimeHours, mGameTimeDays, mGameTimeMarsYears, mSpeed;
+	unsigned long mOldTime;
+};
